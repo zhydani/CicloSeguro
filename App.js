cb74ab9a421aaaf9073f8ac0c064acec823ebceb
Zhydani <zhydani@gmail.com>
2023-04-24 08:56:35 -0300

    convertendo o projeto "https://github.com/zhydani/tcc" para expo

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Teste</Text>
       <StatusBar style="auto" />
     </View>
   );
